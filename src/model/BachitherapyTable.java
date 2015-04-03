38d834a3e87e53a329f65b61db34494d38c22c9f
Administrator <admin@example.com>
2015-04-03 16:33:31 +0530

    Initial commit

---
+++ src/model/BachitherapyTable.java
@@ -0,0 +1,25 @@
+package model;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import sun.java2d.pipe.SpanShapeRenderer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by buddhika on 4/3/15.
+ */
+public class BachitherapyTable {
+    SimpleDateFormat date;
+    SimpleIntegerProperty icrNo;
+    SimpleStringProperty patientName;
+    SimpleIntegerProperty age;
+    SimpleStringProperty clinicNo;
+    SimpleStringProperty Address;
+    SimpleIntegerProperty tpNo;
+    SimpleDateFormat date1;
+    SimpleDateFormat date2;
+    SimpleDateFormat date3;
+    SimpleStringProperty remarks;
+}
